43fe2979b15a610a44f7dfca92d7e8cb93302fd9
Ali Akbar <mail.septiandri@gmail.com>
2016-02-16 12:01:14 +0000

    gulp-data added to list patients

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
+var data = require('gulp-data');
 var jade = require('gulp-jade');
 
 gulp.task('jade', function(event) {
     return gulp.src("jade/*.jade")
+        .pipe(data(function(file) {
+            return require('./dist/patients.json')
+        }))
         .pipe(jade({pretty: true}))
         .pipe(gulp.dest("."))
 });
